788b73dafc7d4aef387bc2a26119971b5dcfe68f
AFODC-708191223\Administrator <384868059@qq.com>
2018-09-10 22:46:05 +0800

    feat(路由搭建)

---
+++ vue-ele/src/components/goods/goods.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		goods
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	
+	
+</style>
--- vue-ele/src/router/index.js
+++ vue-ele/src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import goods from '@/components/goods/goods'
+import seller from '@/components/seller/seller'
+import ratings from '@/components/ratings/ratings'
 Vue.use(Router)
 
 export default new Router({
+	mode:"history",
   routes: [
+		{
+			path: '/',
+			component: goods
+		},
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/goods',
+      name: 'goods',
+      component: goods
+    },
+		{
+			path: "/seller",
+			name: 'seller',
+			component: seller
+		},
+		{
+			path: "/ratings",
+			name: 'ratings',
+			component: ratings
+		}
   ]
 })
