8bf937f63aa06d9db99ce373522a9f9d789bb74e
AFODC-708191223\Administrator <384868059@qq.com>
2018-09-10 20:37:58 +0800

    feat(基础搭建)

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  		<router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Vue from "vue"
+
+export default {
+  name: 'App'
+ 
+}
+
+</script>
+
+<style>
+#app {
+ 
+}
+
+
+</style>
