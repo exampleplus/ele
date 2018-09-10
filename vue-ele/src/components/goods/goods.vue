caac6292b051d0e150558eaccefaf837784cfed4
AFODC-708191223\Administrator <384868059@qq.com>
2018-09-10 23:06:37 +0800

    feat(安装stylus)

--- vue-ele/src/components/goods/goods.vue
+++ vue-ele/src/components/goods/goods.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="goods">
 		goods
 	</div>
 </template>
@@ -8,7 +8,19 @@
 	
 </script>
 
-<style>
-	
+<style lang="stylus" rel="stylesheet/stylus">
+	.goods
+		position:absolute
+		top:180px
+		bottom:46px
+		display:flex
+		width:100%
+		overflow:hidden
+		.menu-wrapper{
+			// flex布局左栏固定
+			flex:0 0 80px
+			width:80px
+			background-color:#f4f5f7
+		}
 	
 </style>
