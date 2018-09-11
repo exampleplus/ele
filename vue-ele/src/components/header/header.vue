ae51f7a83ebf0268b0f86665af6fd94fb8ee4404
AFODC-708191223\Administrator <384868059@qq.com>
2018-09-12 00:06:46 +0800

    feat(header组件部分样式编写)

--- vue-ele/src/components/header/header.vue
+++ vue-ele/src/components/header/header.vue
@@ -13,7 +13,7 @@
 					{{seller.description}}/{{seller.deliveryTime}}分钟送达
 				</div>
 				<div v-if="seller.supports" class="support">
-					<span class="icon"></span>
+					<span class="icon" :class="classMap[seller.supports[0].type]"></span>
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
@@ -24,34 +24,73 @@
 		</div>
 		<!-- 公告 -->
 		<div class="bulletin-wrapper">
-			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+			<span class="bulletin-title"></span>
+			<span class="bulletin-text">{{seller.bulletin}}</span>
 			<i class="icon-keyboard_arrow_right"></i>
 		</div>
 	</div>
 </template>
 <script>
 	export default {
-		props:{
+		props: {
 			seller: {
-				type:Object,
+				type: Object,
 				default: {
-					
+
 				}
 			}
 		},
 		data() {
 			return {
-				
+				classMap:['decrease','discount','guarantee','invoice','special']
 			}
 		}
 	}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+	@import '../../common/stylus/mixin.styl'
 	.header
+		color:#fff
+		position:relative
+		background: #999
 		.content-wrapper
-			position: relative
+			position: relative 
 			padding: 24px 12px 18px 24px
-			
-	
+			.avatar
+				display:inline-block
+			.content
+				display:inline-block
+				margin-left:16px
+			.title
+				margin:2px 0 8px 0
+				.brand
+					display:inline-block;
+					width:30px;
+					height:18px;
+					vertical-align:bottom
+					bg-image('brand')
+					background-size:30px 18px
+					background-repeat:no-repeat
+				.name
+					margin-left:6px
+					font-size:16px
+					font-weight:bold
+					line-height:8px
+			.description
+					margin-bottom:10px
+					line-height:12px
+					font-size:12px
+			.support
+				font-size:10px
+				line-height:12px
+				.icon
+					display:inline-block
+					vertical-align:top
+					width:20px
+					height:20px
+					margin-right:4px
+					bg-image('decrease_1')
+				  background-size: 12px 12px
+		      background-repeat: no-repeat
 </style>
